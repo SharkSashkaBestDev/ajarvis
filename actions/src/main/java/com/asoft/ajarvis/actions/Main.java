7aa8d4de4d7cd229ec4779072827496216d4f63b
Alexander <myagkiyalexandr@hotmail.com>
2019-03-08 22:05:40 +0200

    Multi module relations implemented
    J-1 module depends on Actions module

--- actions/src/main/java/com/asoft/ajarvis/actions/Main.java
+++ actions/src/main/java/com/asoft/ajarvis/actions/Main.java
@@ -2,6 +2,6 @@ package com.asoft.ajarvis.actions;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello");
     }
 }
--- j-1/src/main/java/com/asoft/ajarvis/j1/Main.java
+++ j-1/src/main/java/com/asoft/ajarvis/j1/Main.java
@@ -2,6 +2,6 @@ package com.asoft.ajarvis.j1;
 
 public class Main {
     public static void main(String[] args) {
-
+        com.asoft.ajarvis.actions.Main.main(args);
     }
 }
