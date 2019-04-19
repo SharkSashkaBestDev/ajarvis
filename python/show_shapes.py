4a547eaccfc43c5db8c5005d4ac660b1a4142283
Kir Ryaz <kudgey@mail.ru>
2019-04-19 12:18:17 +0300

    Добавлена команда открытия файла с найденными областями

---
+++ python/show_shapes.py
@@ -0,0 +1,33 @@
+from multiprocessing import Process
+
+def show_shapes(data):
+    print("Show shapes")
+
+    if 'img' in data:
+        data['code'] = '''
+    import cv2;
+    import matplotlib.pyplot as plt;
+    img = cv2.imread(data['img']);
+    img = cv2.resize(img, (int(img.shape[1]/1.1), int(img.shape[0]/1.1)));
+    img = img[:, :, ::-1];
+    dpi = 120;
+    height, width, _ = img.shape;
+
+    figsize = width / float(dpi), height / float(dpi);
+
+    fig = plt.figure(figsize=figsize);
+    ax = fig.add_axes([0, 0, 1, 1]);
+
+    ax.axis('off');
+
+    ax.imshow(img);
+    figManager = plt.get_current_fig_manager();
+    figManager.full_screen_toggle();
+    plt.show()
+    '''
+        process = Process(target=data['temp'], args=(data,))
+        process.start()
+        data['img_pid'] = process.pid
+    else:
+        data['error'] = 'Ничего не нашел, поэтому нечего показывать'
+    return data
