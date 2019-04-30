6d93d6a72107c4047da1bdbe9aa6054200077471
Alexander <myagkiyalexandr@hotmail.com>
2019-04-30 22:16:22 +0300

    [AJ-11] Минорные фиксы по коду

--- recognition/src/main/python/main.py
+++ recognition/src/main/python/main.py
@@ -4,23 +4,28 @@ from threading import Thread
 from queue import Queue
 import requests
 
-
+LANG_RU = "ru-RU"
+PHRASE = "phrase"
+INT = "int"
+STRING = "String"
+BASE_URL = "http://127.0 0.1:8091/ajarvis/commands/"
 
 def set_args(args):
     global args_flag
     cords = []
-    if args["phrase"] != None: #and args["phrase"] != ):
-        #print(args)
-        dict_of_new_phrases = {"phrase": args.pop("phrase")}
+    dict_of_new_phrases = {}
+
+    if args[PHRASE] is not None:
+        dict_of_new_phrases = {PHRASE: args.pop(PHRASE)}
 
         for i in args:
             print("Введите аргумент " + i)
-            args_flag = False;
+            args_flag = False
             if i == "xy":
-                for h in args[i]:
+                for _ in args[i]:
                     while True:
                         try:
-                            arg = (int)(r.recognize_google(list_of_args.get(), language="ru-RU").lower())
+                            arg = int(r.recognize_google(list_of_args.get(), language=LANG_RU).lower())
                             print(arg)
                             cords.append(arg)
                             break
@@ -28,20 +33,20 @@ def set_args(args):
                             print("Повторите попытку ввода")
                 dict_of_new_phrases.update({i: cords})
                 print(dict_of_new_phrases)
-            if args[i] == "int":
+            if args[i] == INT:
                 while True:
                     try:
-                        arg = (int)(r.recognize_google(list_of_args.get(), language="ru-RU").lower())
+                        arg = int(r.recognize_google(list_of_args.get(), language=LANG_RU).lower())
                         print(arg)
                         dict_of_new_phrases.update({i: arg})
                         break
                     except:
                         print("Повторите попытку ввода")
 
-            elif args[i] == "String":
+            elif args[i] == STRING:
                 while True:
-                    arg = r.recognize_google(list_of_args.get(), language="ru-RU").lower()
-                    if(arg != ""):
+                    arg = r.recognize_google(list_of_args.get(), language=LANG_RU).lower()
+                    if arg != "":
                         print(arg)
                         dict_of_new_phrases.update({i: arg})
                         break
@@ -52,15 +57,13 @@ def set_args(args):
     return dict_of_new_phrases
 
 
-
-
 def listen():
     global args_flag
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source, duration=1)
         print("Я слушаю!")
         while True:
-            aud = r.listen(source, phrase_time_limit = 5)
+            aud = r.listen(source, phrase_time_limit=5)
             if args_flag:
                 list_of_audio.put(aud)
             else:
@@ -68,19 +71,17 @@ def listen():
 
 
 def recognize():
-    url = 'http://127.0 0.1:8091/ajarvis/commands/filter'
-    url_param = 'http://127.0 0.1:8091/ajarvis/commands/execute'
-
+    url = BASE_URL + "filter"
+    url_param = BASE_URL + "execute"
 
     while True:
         try:
-            current_phrase = r.recognize_google(list_of_audio.get(), language="ru-RU").lower()
-            #frazees.append(current_phrase)
+            current_phrase = r.recognize_google(list_of_audio.get(), language=LANG_RU).lower()
 
             resoult = standard_phrases(current_phrase, )
-            if (resoult != " "):
+            if resoult != " ":
                 print(resoult)
-                var = requests.post(url, json={"phrase": resoult})
+                var = requests.post(url, json={PHRASE: resoult})
                 send_args = var.json()
 
                 print(send_args)
@@ -94,14 +95,8 @@ def recognize():
 
                 print(get_args)
 
-            else:
-                pass
-
-
-
         except Exception as e:
             print(e)
-            pass
         finally:
             list_of_audio.task_done()
 
@@ -116,36 +111,33 @@ def standard_phrases(current_phrase):
         else:
             print("Завершение отменено")
             end = False
-            return (" ")
+            return " "
 
     if "завершить" in current_phrase.split(" ") and pause_flag:
-        print ("Вы точно хотите выйти")
+        print("Вы точно хотите выйти")
         end = True
-        return (" ")
+        return " "
     if "остановить" in current_phrase.split(" ") and pause_flag:
         pause_flag = False
-        print ("Распознавание приостановлено!")
+        print("Распознавание приостановлено!")
     elif "начать" in current_phrase.split(" ") and not pause_flag:
         pause_flag = True
-        print ("Распознавание возобновлено!")
-        return (" ")
+        print("Распознавание возобновлено!")
+        return " "
     if pause_flag:
         return current_phrase
     else:
-        return (" ");
+        return " "
 
 
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     global end, pause_flag, args_flag
     args_flag = True
     end = False
     pause_flag = True
+
     r = sr.Recognizer()
-    r.energy_threshold = 3500  #уровень чувствительности
-    #r.operation_timeout = 5;
+    r.energy_threshold = 3500  # уровень чувствительности
     current_phrase = ""
 
     list_of_audio = Queue()
@@ -154,6 +146,5 @@ if __name__=="__main__":
     th_1 = Thread(target=listen)
     th_1.setDaemon(True)
     th_1.start()
-
 
     recognize()
